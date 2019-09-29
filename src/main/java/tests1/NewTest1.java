15d328964acfc1379c1be14ab4f2c325d1083c5f
pth_lien <13520439.uit@gmail.com>
2019-09-29 12:36:39 +0700

    Upload to GitHub

---
+++ src/main/java/tests1/NewTest1.java
@@ -0,0 +1,29 @@
+package tests1;
+ 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+ 
+public class NewTest1 {
+ 
+ public WebDriver driver;
+ 
+  @Test
+  public void openMyBlog() {
+ driver.get("https://www.facebook.com");
+  }
+  
+  @BeforeClass
+  public void beforeClass() {
+   
+   System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+   driver = new ChromeDriver();
+   
+  }
+ 
+  @AfterClass
+  public void afterClass() {
+   driver.quit();
+  }
+ 
+}
